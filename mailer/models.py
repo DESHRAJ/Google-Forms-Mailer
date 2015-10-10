87e0bdad65e0c2151867c70594557586f9c6afa8
deshraj <deshrajdry@gmail.com>
2015-10-11 00:44:55 +0530

    views and templates added in mailer app

--- mailer/models.py
+++ mailer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class MailTemplate(models.Model):
+	message = models.TextField(default = None)
+
+class Person(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	name = models.CharField(default = None)
+
--- mailer/views.py
+++ mailer/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import csv
 
 # Create your views here.
+def home(request):
+	'''
+	Home page view 
+	'''
+	template = "index.html"
+	return render(request, template, {})
+
+def mailer(request, template = "mailer.html"):
+	if request.method == "POST":
+		
+	return render(request, template, {})
+
