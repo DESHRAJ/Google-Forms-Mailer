98fe543dd7e62ce5941eaff703289801fb59908a
deshraj <deshrajdry@gmail.com>
2015-10-11 02:42:34 +0530

    routes modified

--- google_forms_mailer/urls.py
+++ google_forms_mailer/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from mailer.views import mailer, home
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'google_forms_mailer.views.home', name='home'),
+    url(r'^$', 'mailer.views.home', name='home'),
+    url(r'^mailer$', 'mailer.views.mailer', name='mailer'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 ]
